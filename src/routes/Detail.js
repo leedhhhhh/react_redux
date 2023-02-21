291de0d1f2a98f6b1c1abf825c5ab73cee187399
dh.lee <dh.lee@twolinecode.com>
2023-02-21 17:45:45 +0900

    feat: 리스트 클릭시 나오는 페이지 추가

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,17 @@
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
 const Detail = () => {
-  return "Detail";
+  const id = useParams().id;
+  const toDos = useSelector((state) => state).find(
+    (v) => v.id === parseInt(id)
+  );
+  return (
+    <>
+      <h1>{toDos.text}</h1>
+      <h5>Create at : {toDos?.id}</h5>
+    </>
+  );
 };
 
 export default Detail;
